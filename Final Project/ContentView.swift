0f5c3e6a844d3c6b38a8558cc9d6304a9f456c61
Natalie Ju <natalieju8@gmail.com>
2024-07-31 18:46:41 -0500

    about page added, main page buttons added, court cases page added

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -9,10 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Color(red:12/256, green:36/256, blue:77/256)
-                .ignoresSafeArea()
-            Image("flag")
+        NavigationStack{
+            ZStack {
+                Color(red:12/256, green:36/256, blue:77/256)
+                    .ignoresSafeArea()
+                Image("flag")
+                    .padding()
+                    .offset(y: -270)
+                Spacer()
+                //Title
+                Text("DemocraTECH")
+                    .font(Font.custom("PlayfairDisplay-Bold", size:55))
+                    .foregroundColor(Color.white)
+                    .offset(y:-80)
+                VStack{
+                    //Description
+                    Text("Take the first step in being an educated voter.\n Click to explore more")
+                        .foregroundColor(Color.white)
+                        .font(Font.custom("PlayfairDisplay-Regular", size:15))
+                        .multilineTextAlignment(.center)
+                        .offset(y:50)
+                    //Court Cases + Representatives button
+                    HStack{
+                        //Court Cases button + text
+                        VStack{
+                            NavigationLink(destination: Court_Cases()){
+                                Image("gavel")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width:100, height:100)
+                            }
+                            
+                            Text("Court Cases")
+                                .foregroundColor(Color.white)
+                                .font(Font.custom("PlayfairDisplay-Regular", size:20))
+                        }
+                        .offset(x:-35,y:50)
+                        //Representatives button + text
+                        VStack{
+                            NavigationLink(destination: Representatives()){
+                                Image("person")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width:100, height:100)
+                            }
+                                                        Text("Representatives")
+                                .foregroundColor(Color.white)
+                                .font(Font.custom("PlayfairDisplay-Regular", size:20))
+                                
+                        }
+                        .offset(x:50,y:50)
+
+                    }
+                    HStack{
+                        
+                    }
+                }
+            }
         }
     }
 }
---
+++ Final Project/Court Cases.swift
@@ -0,0 +1,106 @@
+//
+//  Court Cases.swift
+//  Final Project
+//
+//  Created by Scholar on 7/31/24.
+//
+
+import SwiftUI
+
+struct Court_Cases: View {
+    var body: some View {
+        NavigationStack{
+          ZStack{
+            Color(red: 12/256, green: 36/256, blue: 77/256)
+              .ignoresSafeArea()
+            VStack{
+              Text("Court Cases")
+                .font(Font.custom("PlayfairDisplay-Regular", size:60))
+              //  .font(.system(size: 65))
+              //  .font(.title)
+                .foregroundColor(.white)
+                .offset(y: -10)
+              Text("Recent Cases")
+                .foregroundColor(.white)
+                .multilineTextAlignment(.leading)
+                .padding(.trailing, 155)
+                .font(Font.custom("PlayfairDisplay-Regular", size:35))
+              HStack{
+                VStack{
+                  Image("courtCase")
+                    .resizable()
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                  NavigationLink(destination: trumpUS()) {
+                    Text("Trump vs \n United States ")
+                      .foregroundColor(.white)
+                      .multilineTextAlignment(.center)
+                      .font(Font.custom("PlayfairDisplay-Regular", size:25))
+                  }
+                }
+                .padding(15)
+                //  .padding(.bottom, 15)
+                VStack{
+                  Image("fischer")
+                    .resizable()
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                  NavigationLink(destination: fischer()) {
+                    Text("Fischer vs \n United States")
+                      .foregroundColor(.white)
+                      .multilineTextAlignment(.center)
+                      .font(Font.custom("PlayfairDisplay-Regular", size:25))              .padding(.top, 2)
+                  }
+                }
+                .padding(1)
+              }
+              .padding(1)
+              //Color(red: 125/256, green: 0/256, blue: 17/256)
+              .background(Rectangle() .foregroundColor(Color(red: 0.49, green: 0.0, blue: 0.0)))
+              .cornerRadius(15)
+              //.padding(.top, 0)
+              Text("Major Historical Cases")
+                .foregroundColor(.white)
+                .multilineTextAlignment(.leading)
+                .padding(.trailing, 17)
+                .font(Font.custom("PlayfairDisplay-Regular", size:35))          //   .padding(.top, 10)
+              HStack{
+                VStack{
+                  Image("marbury")
+                    .resizable()
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                  NavigationLink(destination: marbury()) {
+                    Text("Marbury vs \n Madison")
+                      .foregroundColor(.white)
+                      .multilineTextAlignment(.center)
+                      .font(Font.custom("PlayfairDisplay-Regular", size:25))
+                  }
+                }
+                .padding(12.0)
+                VStack{
+                  Image("roe")
+                    .resizable()
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                  NavigationLink(destination: Roe()) {
+                    Text("Roe vs \n Wade")
+                      .foregroundColor(.white)
+                      .multilineTextAlignment(.center)
+                      .font(Font.custom("PlayfairDisplay-Regular", size:25))
+                  }
+                }
+                .padding(12.0)
+              }
+              .background(Rectangle() .foregroundColor(Color(red: 0.49, green: 0.0, blue: 0.0)))
+              .cornerRadius(15)
+            }
+          }
+        }
+      }
+    }
+}
+
+#Preview {
+    Court_Cases()
+}
